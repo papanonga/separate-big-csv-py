97957403279d90d665e5c50d96dd7759baaf8fa7
God <kimjonggod@hotmail.com>
2022-06-14 12:26:31 +0700

    Fix bug waiting for create files

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import time
 from datetime import date
 from fileinput import filename
 import timeit
@@ -8,24 +9,19 @@ chunksize = 10 ** 8
 
 def getSpecifiedDataFromChunk(data):
     case = {
-        "date" : data['TextTime'].split(' ')[0],
-        "province" : data['NE Name'][:3]
+        "date": data['TextTime'].split(' ')[0],
+        "province": data['NE Name'][:3]
     }
     # return f'{}-{}.csv'
     return f'LTE KPI Backup EAS {case.get("province")}&{case.get("date")}.csv'
 
 
-
-
-
-
 def main():
 
     # Edit here
-    pathForSearch = 'Enter your path here'
+    pathForSearch = './data/LTE KPI Backup EAS 20220221.csv'
 
-
-
+    fileNameList = []
     start = timeit.default_timer()
     result = []
     dateWithProvinceList = []
@@ -37,46 +33,77 @@ def main():
     for i in range(len(result[0])):
         chunkDate = result[0].iloc[i]['TextTime'].split(' ')[0]
         chunkProvince = result[0].iloc[i]['NE Name'][:3]
-        if [chunkDate,chunkProvince] not in dateWithProvinceList:
+        if [chunkDate, chunkProvince] not in dateWithProvinceList:
             dateWithProvinceList.append([chunkDate, chunkProvince])
 
+    print("Chunk date,province = ", dateWithProvinceList)
     # create file named date and generate the header
+    print("Create file")
+    print(getSpecifiedDataFromChunk(data=result[0].iloc[0]))
+
     for i in range(len(dateWithProvinceList)):
         fileName = getSpecifiedDataFromChunk(data=result[0].iloc[i])
-        if path.isfile(f'./result/{fileName}'):
+        pathForChecking = f'./result/{fileName}'
+        if path.isfile(pathForChecking):
             pass
 
         else:
-            f = open(f'./result/{fileName}', "x")
-            lengthResult = len(result[0].columns)
-            for i in range(lengthResult):
-                f.write(f'{result[0].columns[i]}')
-                if i == len(result[0].columns) - 1:
-                    continue
-                f.write(',')
+            with open(pathForChecking, "x") as f:
+                lengthResult = len(result[0].columns)
+                for i in range(lengthResult):
+                    f.write(f'{result[0].columns[i]}')
+                    if i == len(result[0].columns) - 1:
+                        f.write('\n')
+                        continue
+                    f.write(',')
+                    
+
+
+    print('Waiting create file')
+    indexFileNameList = 0
+
+    # while True:
+
+    #     if indexFileNameList == len(fileNameList):
+    #         print("Finished")
+    #         break
+
+    #     if path.exists(fileNameList[indexFileNameList]):
+    #         time.sleep(1)
+    #     else:
+    #         indexFileNameList += 1
+
+    # print('create finished')
 
     countWriter = 0
+
     # # read write file
+    print('Read wirte files : ', len(result[0]))
+
+    oldPath = getSpecifiedDataFromChunk(data=result[0].iloc[0])
+    countWrongPath = 0
+
     for i in range(len(result[0])):
-        dateWithProvinceOfChunk = getSpecifiedDataFromChunk(data=result[0].iloc[i])
+        dateWithProvinceOfChunk = getSpecifiedDataFromChunk(
+            data=result[0].iloc[i])
+        if oldPath != dateWithProvinceOfChunk:
+            countWrongPath += 1
+            oldPath = dateWithProvinceOfChunk
 
         if path.isfile(f'./result/{dateWithProvinceOfChunk}'):
-            result[0].iloc[[i]].to_csv(f'./result/{dateWithProvinceOfChunk}', index=False, header=False, mode='a')
+            result[0].iloc[[i]].to_csv(
+                f'./result/{dateWithProvinceOfChunk}', index=False, header=False, mode='a')
             countWriter += 1
 
     print('Len files : ', len(result[0]))
-    print('CountWriterFile : ', countWriter)
     stop = timeit.default_timer()
-    print("Run time :", int(stop-start) // 60 ," min ", int((stop-start) % 60)  , ' sec ')
+    print("Run time :", int(stop-start) // 60,
+          " min ", int((stop-start) % 60), ' sec ')
+    print("Wrong path : ", countWrongPath)
     print('End')
+
     return
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
